a5dcfffe35c7e80d22e8e8b90c325b0fa98d193b
TheAlchemist0033 <36774748+TheAlchemist0033@users.noreply.github.com>
2021-04-21 12:50:07 -0700

    Create items.lua

---
+++ items.lua
@@ -0,0 +1,28 @@
+minetest.register_craftitem(
+        "alchemy:eye_of_earth",
+        {
+            description = "Eye of Earth",
+            inventory_image = "alchemy_earth_eye.png",
+        }
+)
+minetest.register_craftitem(
+        "alchemy:eye_of_sand",
+        {
+            description = "Eye of sand",
+            inventory_image = "alchemy_sand_eye.png",
+        }
+)
+minetest.register_craftitem(
+        "alchemy:arboreal_tooth",
+        {
+            description = "Arboreal tooth",
+            inventory_image = "alchemy_arboreal_tooth.png",
+        }
+)
+minetest.register_craftitem(
+        "alchemy:essence_of_fire",
+        {
+            description = "Essence of Fire",
+            inventory_image = "alchemy_fire_essence.png",
+        }
+)
