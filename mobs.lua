165169e9993b969d048e6cb37aeae8a137364821
TheAlchemist0033 <36774748+TheAlchemist0033@users.noreply.github.com>
2021-04-21 13:02:03 -0700

    Update mobs.lua

--- mobs.lua
+++ mobs.lua
@@ -2,15 +2,14 @@
 --The MIT License (MIT) (for sand monster)
 --Copyright (c) 2016 TenPlus1 (for sand monster)
 local S = mobs.intllib
-local stone_types = {
 
-    {	nodes = {"default:desert_stone"},
-         skins = {"mobs_stone_monster3.png"},
+local dirt_types = {
+
+    {	nodes = {"ethereal:dry_dirt"},
+         skins = {"mobs_dirt_monster3.png"},
          drops = {
-             {name = "default:desert_cobble", chance = 1, min = 0, max = 2},
-             {name = "default:iron_lump", chance = 5, min = 0, max = 2},
-             {name = "default:gold_lump", chance = 5, min = 0, max = 2},
-             {name = "alchemy:eye_of_earth", chance = 25, min = 0, max = 2}
+             {name = "ethereal:dry_dirt", chance = 1, min = 0, max = 2},
+             {name = "alchemy:eye_of_earth",chance=23,min = 1, max = 3}
          }
     }
 }
@@ -147,44 +146,42 @@ mobs:register_mob("alchemy:sand_monster", {
 })
 
 
--- Stone Monster by PilzAdam
+-- Dirt Monster by PilzAdam
 
-mobs:register_mob("alchemy:stone_monster", {
+mobs:register_mob("alchemy:dirt_monster", {
     type = "monster",
     passive = false,
     attack_type = "dogfight",
     pathfinding = true,
     reach = 2,
-    damage = 3,
-    hp_min = 12,
-    hp_max = 35,
-    armor = 80,
-    collisionbox = {-0.4, -1, -0.4, 0.4, 0.9, 0.4},
+    damage = 2,
+    hp_min = 3,
+    hp_max = 27,
+    armor = 100,
+    collisionbox = {-0.4, -1, -0.4, 0.4, 0.8, 0.4},
     visual = "mesh",
     mesh = "mobs_stone_monster.b3d",
     textures = {
-        {"mobs_stone_monster.png"},
-        {"mobs_stone_monster2.png"}, -- by AMMOnym
+        {"mobs_dirt_monster.png"},
+        {"mobs_dirt_monster2.png"},
     },
+    blood_texture = "default_dirt.png",
     makes_footstep_sound = true,
     sounds = {
-        random = "mobs_stonemonster",
+        random = "mobs_dirtmonster",
     },
+    view_range = 15,
     walk_velocity = 1,
-    run_velocity = 2,
-    jump_height = 0,
-    stepheight = 1.1,
-    floats = 0,
-    view_range = 10,
+    run_velocity = 3,
+    jump = true,
     drops = {
-        {name = "default:cobble", chance = 1, min = 0, max = 2},
-        {name = "default:coal_lump", chance = 3, min = 0, max = 2},
-        {name = "default:iron_lump", chance = 5, min = 0, max = 2},
-        {name = "alchemy:eye_of_earth", chance = 25, min = 1, max = 2}
+        {name = "default:dirt", chance = 1, min = 0, max = 2},
+        {name = "alchemy:eye_of_earth", chance = 23, min = 1, max = 3}
     },
-    water_damage = 0,
-    lava_damage = 1,
-    light_damage = 0,
+    water_damage = 1,
+    lava_damage = 5,
+    light_damage = 3,
+    fear_height = 4,
     animation = {
         speed_normal = 15,
         speed_run = 15,
@@ -197,24 +194,16 @@ mobs:register_mob("alchemy:stone_monster", {
         punch_start = 40,
         punch_end = 63,
     },
-    immune_to = {
-        {"default:pick_wood", 0}, -- wooden pick doesnt hurt stone monster
-        {"default:pick_stone", 4}, -- picks deal more damage to stone monster
-        {"default:pick_bronze", 5},
-        {"default:pick_steel", 5},
-        {"default:pick_mese", 6},
-        {"default:pick_diamond", 7},
-    },
 
-    -- check surrounding nodes and spawn a specific spider
+    -- check surrounding nodes and spawn a specific monster
     on_spawn = function(self)
 
         local pos = self.object:get_pos() ; pos.y = pos.y - 1
         local tmp
 
-        for n = 1, #stone_types do
+        for n = 1, #dirt_types do
 
-            tmp = stone_types[n]
+            tmp = dirt_types[n]
 
             if minetest.find_node_near(pos, 1, tmp.nodes) then
 
@@ -232,7 +221,7 @@ mobs:register_mob("alchemy:stone_monster", {
         return true -- run only once, false/nil runs every activation
     end
 })
--- Tree Monster (or Tree Gollum) by PilzAdam
+
 
 mobs:register_mob("alchemy:tree_monster", {
     type = "monster",
@@ -414,11 +403,14 @@ mobs:register_mob("alchemy:lava_flan", {
 
 if not mobs.custom_spawn_monster then
     mobs:spawn({
-        name = "alchemy:stone_monster",
-        nodes = {"default:stone", "default:desert_stone", "default:sandstone"},
+        name = "mobs_monster:dirt_monster",
+        nodes = {"default:dirt_with_grass", "ethereal:gray_dirt", "ethereal:dry_dirt"},
+        min_light = 0,
         max_light = 7,
-        chance = 7000,
-        max_height = 0,
+        chance = 6000,
+        active_object_count = 2,
+        min_height = 0,
+        day_toggle = false,
     })
     mobs:spawn({
         name = "alchemy:sand_monster",
@@ -447,15 +439,15 @@ end
 
 
 mobs:register_egg("alchemy:sand_monster", S("Sand Monster"), "default_desert_sand.png", 1)
-mobs:register_egg("alchemy:stone_monster", S("Stone Monster"), "default_stone.png", 1)
 mobs:register_egg("alchemy:tree_monster", S("Tree Monster"), "default_tree_top.png", 1)
 mobs:register_egg("alchemy:lava_flan", S("Lava Flan"), "default_lava.png", 1)
+mobs:register_egg("alchemy:dirt_monster", S("Dirt Monster"), "default_dirt.png", 1)
 
 --ironically removed alias for compatability
 
 
 ---thank you to TenPlus1 for this source code. Im sorry i pretty much ripped off your source, its only temporary
----until i am able to finish up my own custom mobs. Ive included the copyrite to the original code below. 
+---until i am able to finish up my own custom mobs. Ive included the copyrite to the original code below.
 ---
 ---
 ---The MIT License (MIT)
